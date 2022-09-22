421b4efd3d960c4c882a330371eff10e99748cb0
LWagg <109525643+LWagg@users.noreply.github.com>
2022-09-22 16:47:44 -0300

    Correção exercícios 03

---
+++ aulas/Null e undefined - Aula 03-03/app.js
@@ -0,0 +1,4 @@
+let emptiness = null
+console.log(emptiness, emptiness + 3, `O valor é ${emptiness}`)
+
+//Podemos utilizar o NULL para resetar alguma variável, com objetivo que ela possa receber outro valor se necessário
