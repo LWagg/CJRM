2b7d4cf2d312a685755ccf7b5e9c5a1c63526ae3
LWagg <109525643+LWagg@users.noreply.github.com>
2022-09-23 12:09:30 -0300

    igualdade estrita

---
+++ aulas/Comparações com igualdade estrita - Aula 04-03/app.js
@@ -0,0 +1,10 @@
+const age = 31
+
+// 'igual a' e 'diferente de'
+console.log(age == 31)
+console.log(age == '31') /* usando dois sinais de igual (==), antes de fazer a comparação, o JS converte a string para number, ai teremos TRUE como resultado*/
+
+
+
+// 'igual a, e do mesmo tipo' e 'diferente de, e do mesmo tipo'
+ 
