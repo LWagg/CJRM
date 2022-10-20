24a3d0cb7a5d5429a990d91c9f403c2a2c49ed66
LWagg <109525643+LWagg@users.noreply.github.com>
2022-10-20 14:20:56 -0300

    RegEx

---
+++ aulas/Etapa 06 - Eventos em formulários/Expressões regulares - Aula 01-04/app.js
@@ -0,0 +1,6 @@
+/* Utilizamos expressões regulares (regEx) para validar o que o usuário está digitando nos formulários.
+    [] -> colocamos as letras (maiúsculas e minúsculas) e os números que serão aceitos dentro das chaves.
+    {} -> colocamos o intervalo com a quantidade de caracteres que desejamos dentro dos colchetes.
+    . -> o ponto representa os caracteres especiais.
+    ^ e $ -> o acento e o cifrão indicam que não serão aceitos palavras separadas.
+[*/
