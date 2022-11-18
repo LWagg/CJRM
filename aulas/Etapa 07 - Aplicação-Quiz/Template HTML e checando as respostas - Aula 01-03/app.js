f6f451f9aa1659de256f6b8ca168f63a9a7059e8
LWagg <109525643+LWagg@users.noreply.github.com>
2022-11-18 11:29:05 -0300

    quiz

--- aulas/Etapa 07 - Aplicação-Quiz/Template HTML e checando as respostas - Aula 01-03/app.js
+++ aulas/Etapa 07 - Aplicação-Quiz/Template HTML e checando as respostas - Aula 01-03/app.js
@@ -1,29 +1,41 @@
 const form = document.querySelector('.quiz-form')
-const finalResult = document.querySelector('.result')
+const finalScoreContainer = document.querySelector('.final-score-container')
 
-const correctAnwers = ['B', 'B', 'B', 'B']
+const correctAnswers = ['D', 'C', 'A', 'B']
 
-form.addEventListener('submit', event => {
-    event.preventDefault()
+let score = 0
 
-    let score = 0
-    const userAnswers = [
-        form.inputQuestion1.value,
-        form.inputQuestion2.value,
-        form.inputQuestion3.value,
-        form.inputQuestion4.value,
-    ]
+const getUsersAnwers = () => {
+    let userAnswers = [] 
 
+    correctAnswers.forEach((_, index) => {
+        const userAnswer = form[`inputQuestion${index + 1}`].value
+        userAnswers.push(userAnswer)
+    })
+
+     return userAnswers
+}
+
+const calculateUserScore = userAnswers => {
     userAnswers.forEach((userAnswer, index) => {
-        if (userAnswer === correctAnwers[index]) {
+        const isUserAnswerCorrect = userAnswer === correctAnswers[index]
+        
+        if (isUserAnswerCorrect) {
         score += 25
         }
     })
+}
 
-    scrollTo(0, 0)
+const showFinalScore = () => {
+    scrollTo({
+        top: 0,
+        left: 0, 
+        behavior: 'smooth'
+    })
+    finalScoreContainer.classList.remove('d-none')
+}
 
-    finalResult.classList.remove('d-none')
-
+const animateFinalScore = () => {
     let counter = 0
 
     const timer = setInterval (() => {
@@ -32,9 +44,18 @@ form.addEventListener('submit', event => {
             clearInterval(timer)
         }
 
-        finalResult.querySelector('span').textContent = `${counter}%`
-        counter++
+        finalScoreContainer.querySelector('span').textContent = `${counter++}%`
     }, 10)
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const userAnswers = getUsersAnwers()
+
+    calculateUserScore(userAnswers)
+    showFinalScore()
+    animateFinalScore()
 })
 
 
