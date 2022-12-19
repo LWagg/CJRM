c4a90790832c2fc8b4c482f0dd6c2eb372b2c9de
LWagg <109525643+LWagg@users.noreply.github.com>
2022-12-18 21:27:08 -0300

    Refactoring na aplicação

--- aulas/Etapa 08 - Métodos de array/Adicionando to-dos - Aula 01-03/app.js
+++ aulas/Etapa 08 - Métodos de array/Adicionando to-dos - Aula 01-03/app.js
@@ -36,21 +36,38 @@ toDosContainer.addEventListener('click', event => {
     removeToDo(clickedElement)
 })
 
+const filterToDos = (toDos, inputValue, returnMatchedToDos) => toDos
+    .filter(toDo => {
+        const matchedToDos = toDo.textContent.toLowerCase().includes(inputValue)
+        return returnMatchedToDos ? matchedToDos : !matchedToDos
+    })
+
+const manipulateClasses = (toDos, classToAdd, classToRemove) => {
+    toDos.forEach(toDo => {
+        toDo.classList.remove(classToRemove)
+        toDo.classList.add(classToAdd)
+    }) 
+}
+
+
+const hideToDos = (toDos, inputValue) => {
+    const toDosToHide = filterToDos(toDos, inputValue, false)
+    manipulateClasses(toDosToHide, 'hidden', 'd-flex')
+}
+
+const showToDos = (toDos, inputValue) => {
+    const toDosToShow = filterToDos(toDos, inputValue, true)
+    manipulateClasses(toDosToShow, 'd-flex', 'hidden')
+}
+
+
 inputSearchToDo.addEventListener('input', event => {
     const inputValue = event.target.value.trim().toLowerCase()
-    Array.from(toDosContainer.children)
-        .filter(toDo => !toDo.textContent.toLowerCase().includes(inputValue))
-        .forEach(toDo => {
-            toDo.classList.remove('d-flex')
-            toDo.classList.add('hidden')
-        }) 
-    Array.from(toDosContainer.children)
-        .filter(toDo => toDo.textContent.toLowerCase().includes(inputValue))
-        .forEach(toDo => {
-            toDo.classList.remove('hidden')
-            toDo.classList.add('d-flex')
-        }) 
+    const toDos = Array.from(toDosContainer.children)
+
+    hideToDos(toDos, inputValue)
+    showToDos(toDos, inputValue)
 })
 
-// 13: 50
+
 
