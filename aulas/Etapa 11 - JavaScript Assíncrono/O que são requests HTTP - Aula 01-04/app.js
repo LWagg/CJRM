3f3c399925bc95e26fd7f19cd9aa09bd65de9ae7
LWagg <109525643+LWagg@users.noreply.github.com>
2023-01-05 10:33:01 -0300

    HTTP request

---
+++ aulas/Etapa 11 - JavaScript Assíncrono/O que são requests HTTP - Aula 01-04/app.js
@@ -0,0 +1,8 @@
+/*
+    API - Application Programming Interface
+        É uma ferramenta que permite a comunicação entre uma aplicação e outra, permitindo também que uma aplicação possa utilizar recursos dessa outra aplicação.
+
+    Para ocorrer essa comunicação, precisamos fazer uma requisição HTTP (Hypertext Transfer Protocol)
+
+    Essa requisição é feita para um ENDPOINT da API ou banco de dados escolhido. O endpoint contém uma URL que gera uma resposta em formato JSON (JavaScript Object Notation), um formato muito parecido com um objeto JavaScript.
+*/
