11ef8a2c4e56fe2659afb383d7506c7b445e84c0
LWagg <109525643+LWagg@users.noreply.github.com>
2023-01-11 16:46:49 -0300

    exercícios 30

--- exercicios/exercicio-30/app.js
+++ exercicios/exercicio-30/app.js
@@ -8,6 +8,32 @@
     dados dos usuários."
 */
 
+const getUsers = url => new Promise((resolve, reject) => {
+  const request = new XMLHttpRequest()
+
+  request.addEventListener('readystatechange', () => { 
+    const isRequestOk = request.readyState === 4 && request.status === 200
+    const isRequestNotOk = request.readyState === 4
+    
+    if (isRequestOk) {
+        const data = JSON.parse(request.responseText)
+        resolve(data)
+    }
+
+    if (isRequestNotOk) {
+        reject('Não foi possível obter os dados dos usuários')
+    }
+})
+
+request.open('GET', url)
+request.send()
+
+})
+
+getUsers('https://jsonplaceholder.typicode.com/users')
+    .then(user => console.log(user))
+    .catch(error => console.log(error))
+
 /*
   02
 
