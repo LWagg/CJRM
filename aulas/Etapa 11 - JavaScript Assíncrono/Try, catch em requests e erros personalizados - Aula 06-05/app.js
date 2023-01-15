30d5b1af48ce235f72eddac6798a9f3f4f9ae4ad
LWagg <109525643+LWagg@users.noreply.github.com>
2023-01-14 21:33:17 -0300

    try/catch

---
+++ aulas/Etapa 11 - JavaScript Assíncrono/Try, catch em requests e erros personalizados - Aula 06-05/app.js
@@ -0,0 +1,25 @@
+const getUsers = async  () => {
+    try {
+
+        const response = await fetch('./json/todoss.json')
+
+        if(!response.ok) {
+            throw new Error('Não foi possível obter os dados')
+        }
+        
+        return response.json()
+
+    } catch(error) {
+        console.log(error.message)
+    }
+}
+
+const logUsers = async () => {
+    const users = await getUsers()
+    console.log(users)
+}
+
+logUsers()
+
+
+
