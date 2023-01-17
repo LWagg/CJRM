ece1d2bfbd8470b8f2b5b92b3614f2ad9a32e076
LWagg <109525643+LWagg@users.noreply.github.com>
2023-01-17 16:45:32 -0300

    weather app

---
+++ aulas/Etapa 12 - Weather Application/Um preview da Weather Application - Aula 01-04/js/app.js
@@ -0,0 +1,39 @@
+const APIKey = 'SulVDZSD21OP0ps89TTMCoAGojJPKlS8'
+
+const getCityURL = cityName =>  
+    `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+
+const getCityData = async cityName => {
+    try {
+        const cityUrl = getCityURL(cityName)
+        const response = await fetch(cityUrl)
+
+        if(!response.ok) {
+            throw new error('Não foi possível obter os dados')
+        }
+
+        const [cityData] = await response.json()
+        return cityData
+    } catch ({name, message}) {
+        alert(`${name}: ${message}`)
+    }
+}
+
+const getCityWeather = async cityName => {
+    try {
+        const { Key } = await getCityData(cityName)
+        const cityWeatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}`
+        const response = await fetch(cityWeatherUrl)
+
+        if(!response.ok) {
+            throw new error('Não foi possível obter os dados')
+        }
+
+        const [cityData] = await response.json()
+        return cityData
+    } catch ({name, message}) {
+        alert(`${name}: ${message}`)
+    }
+}
+
+getCityData('São Paulo')
