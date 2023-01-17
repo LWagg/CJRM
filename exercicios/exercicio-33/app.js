03453ea4e0fc81ec33cceae8f7623fac0352a5ac
LWagg <109525643+LWagg@users.noreply.github.com>
2023-01-17 18:58:33 -0300

    exercícios 33

--- exercicios/exercicio-33/app.js
+++ exercicios/exercicio-33/app.js
@@ -5,7 +5,7 @@
   - Solucione esse problema sem declarar uma constante "book".
 */
 
-// console.log(book)
+console.log(book)
 
 /*
   02
@@ -16,6 +16,8 @@
     - O 2º item é o 1º item do array recebido por argumento;
   - Implemente a função da forma mais concisa que você conseguir.
 */
+
+
 
 /*
   03
